81cca67fc39291405d6d08ba6d8904ae12ce4a0e
Ogarkov Sergey <coriouse@gmail.com>
2017-03-30 08:33:53 +0300

    added snake

---
+++ pixel-jSnake/src/app/pixel/jsnake/object/Snake.java
@@ -0,0 +1,18 @@
+package app.pixel.jsnake.object;
+
+public class Snake extends Mob {
+
+	public Snake(float posX, float posY) {
+		super(posX, posY);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void update() {
+
+	}
+
+	public void render() {
+
+	}
+
+}
